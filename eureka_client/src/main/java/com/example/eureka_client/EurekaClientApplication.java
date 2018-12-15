15a58beb0064e37df010d33db083c9d53da1bdbf
klskls <1978354075@qq.com>
2018-12-15 21:22:35 +0800

    1215
    1215

---
+++ eureka_client/src/main/java/com/example/eureka_client/EurekaClientApplication.java
@@ -0,0 +1,28 @@
+package com.example.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@GetMapping("asd")
+	public Map<String,String> get(){
+		Map<String,String> map=new HashMap<>();
+		map.put("asd","asd");
+		return map;
+	}
+}
+
